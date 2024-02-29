7f6984b5e3b50099ce75c81530590070cea16321
912410023 <912410023@o365.tku.edu.tw>
2024-02-29 19:34:02 +0800

    ### W02-P1: Show Tictactoe css

---
+++ demo/week2/js/w02_23.js
@@ -0,0 +1,11 @@
+const container=document.querySelector('container');
+const allLiEle=document.querySelectorAll('#board li');
+const resetBtn=document.querySelector('#reset');
+resetBtn.addEventListener('click',function() {
+ 
+});
+
+const playerO='o';
+const playerX='x';
+let turn=0;
+let done=false;
