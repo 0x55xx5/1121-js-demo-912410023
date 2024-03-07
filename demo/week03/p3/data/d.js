194fc8991ec036983ff8990d7c524649f927453f
912410023 <912410023@o365.tku.edu.tw>
2024-03-07 20:11:26 +0800

    W03-P3: import sdata and students data from data_xx.js

---
+++ demo/week03/p3/data/d.js
@@ -0,0 +1,25 @@
+export const scores_data=[90,100,60,40,20,80];
+export const students=[
+
+    {
+        id:1,
+        name:"kevin1",
+        score:80
+    },  {
+        id:2,
+        name:"kevin2",
+        score:90
+    },  {
+        id:3,
+        name:"kevin3",
+        score:50
+    },  {
+        id:4,
+        name:"kevin4",
+        score:30
+    },  {
+        id:5,
+        name:"kevin5",
+        score:100
+    }
+];
---
+++ demo/week03/p3/js/w03_23.js
@@ -0,0 +1,3 @@
+import { scores_data,students } from "../data/d.js";
+console.log(scores_data);
+console.log(students);
