6b01d18d6cb95c969f0362babc43d6cf8dc26bfd
Your Name <you@example.com>
2024-03-21 20:54:50 +0800

    add btns

---
+++ demo/week5/js/5_23.js
@@ -0,0 +1,56 @@
+import { menu } from '../data/d.js';
+console.log(menu);
+const cat = ['all', 'breakfast', 'lunch', 'dinner'];
+const showBtn = () => {
+  const catHtml = cat
+    .map((itm) => {
+      const content = `
+        <button type="button" class="filter-btn" data-id="${itm}">
+        ${itm}
+      </button>
+          `;
+      console.log(content);
+      return content;
+    })
+    .join('');
+
+  const section = document.querySelector('.btn-container');
+  section.innerHTML = catHtml;
+};
+
+const showData = () => {
+  const menuItemHTML = menu
+    .map((itm) => {
+      const { id, category, title, price, img, desc } = itm;
+      const content = `
+        <article class="menu-item">
+        <img
+          src=${img}
+          alt="buttermilk"
+          pancakes=""
+          class="photo"
+        />
+        <div class="item-info">
+          <header>
+            <h4>${title}</h4>
+            <h4 class="price">${price}</h4>
+          </header>
+          <p class="item-text">
+          ${desc}
+          </p>
+        </div>
+      </article>
+        `;
+      console.log(content);
+      return content;
+    })
+    .join('');
+
+  const section = document.querySelector('.section-center');
+  section.innerHTML = menuItemHTML;
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  showData();
+  showBtn();
+});
