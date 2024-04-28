b8efe973194376fb67760e444a8c98d3f56a9509
Kevin Yu <jsyu17@gmail.com>
2024-04-28 23:20:25 +0800

    w10-p4.png

--- demo/week10/p1_xx/p1_xx copy.js
+++ demo/week10/p1_xx/p1_xx copy.js
@@ -1,24 +1,41 @@
 import { _supabase } from './client.js';
 console.log('client.js', _supabase);
 
-let product_23=[];
+let product_23 = [];
 
 const productContainer = document.querySelector('.products-container');
 
 //console.log('products_xx', products_xx);
 
-const DisplayProducts = (products) => {};
-const getProductFromDB = async() => {
+const DisplayProducts = (products) => {
+  let prods = products.map((product) => {
+    const { id, title, img, price } = product;
+    return `<div class="single-product">
+  <img
+          src="${img}"
+          class="single-product-img img"
+          alt="high-back bench"
+        />
+        <footer>
+          <h3 class="name">${title}</h3>
+          <span class="price">${price}</span>
+        </footer>
+  </div>
+  `;
+  }).join('');
+  productContainer.innerHTML = prods;
+}
+const getProductFromDB = async () => {
   try {
-    let{data,error}=await _supabase.from('product_23').select('*');
-    console.log('product_23',data);
+    let { data, error } = await _supabase.from('product_23').select('*');
+    console.log('product_23', data);
     return data;
   } catch (error) {
-    
+
   }
 };
 
-document.addEventListener('DOMContentLoaded',async () => {
-  product_23=await getProductFromDB();
-  DisplayProducts(products_xx);
+document.addEventListener('DOMContentLoaded', async () => {
+  product_23 = await getProductFromDB();
+  DisplayProducts(product_23);
 });
