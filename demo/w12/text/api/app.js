0e8cdaaea29aa5f78797d915ef6f735375083dfa
Kevin Yu <jsyu17@gmail.com>
2024-05-09 19:39:25 +0800

    WP01

---
+++ demo/w12/text/api/app.js
@@ -0,0 +1,44 @@
+
+
+function getData(){
+    const xhr=new XMLHttpRequest();
+    xhr.open('GET','./1.txt',true);
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState===4){
+            console.log(xhr.responseText);
+            const ele=document.createElement('p');
+            ele.textContent=xhr.responseText;
+            document.body.appendChild(ele);
+        }
+        else{
+            console.log("ex:"+xhr.statusText); 
+        }
+    }
+    xhr.send();
+}
+const btn=document.getElementById('btn');
+btn.addEventListener('click',getData);
+/*
+
+const xhr=new XMLHttpRequest();
+console.log(xhr.UNSENT);
+console.log(xhr.OPENED);
+xhr.open('GET','./1.txt',true);
+console.log(xhr.OPENED);
+
+xhr.onreadystatechange=function(){
+    console.log("xhr.readyState"); 
+    console.log(xhr.readyState);
+    if(xhr.readyState===4){
+        console.log(xhr.responseText);
+        const ele=document.createElement('p');
+        ele.textContent=xhr.responseText;
+        document.body.appendChild(ele);
+    }
+    else{
+        console.log("ex:"+xhr.statusText); 
+    }
+}
+xhr.send();
+
+*/
